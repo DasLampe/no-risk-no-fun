218cb8bf68373d6349cd0fac471d81046799748d
DasLampe <daslampe@lano-crew.org>
2011-06-02 21:20:39 +0200

    Fix Bug: Lineende (v.2)

---
+++ army.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class army:
+	def __init__(self, countries):
+		self.army2country	= countries.get_country_list()
+	
+	def set_army(self, country_id, number):
+		if len(self.army2country[country_id]) == 2:
+			self.army2country[country_id].append(number)
+		else:
+			self.army2country[country_id][2]	+= number
+			
+	def get_army(self, country_id):
+		if len(self.army2country[country_id]) == 2:
+			return 0
+		else:
+			return self.army2country[country_id][2]
